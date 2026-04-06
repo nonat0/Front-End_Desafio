76e95587dd0d73ab107efd437dff3e6f4138e210
nonat0 <irafaelnonato@gmail.com>
2026-04-06 00:41:25 -0300

    Correções finais na página de detalhes do produto (as alterações de estilo de botão não estavam sendo refletidas nela)

--- Sprint 1/src/pages/ProductDetail/ProductDetail.jsx
+++ Sprint 1/src/pages/ProductDetail/ProductDetail.jsx
@@ -148,7 +148,7 @@ export function ProductDetail() {
                 aria-label={inWatchlist ? 'Remover da Watchlist' : 'Adicionar à Watchlist'}
               >
                 <svg width="18" height="18" viewBox="0 0 24 24" fill={inWatchlist ? 'currentColor' : 'none'} stroke="currentColor" strokeWidth="2">
-                  <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
+                  <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/>
                 </svg>
                 {inWatchlist ? 'Na Watchlist' : 'Watchlist'}
               </button>
